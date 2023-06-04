8577e92b0470c453f97b4aaacd4564e3e09cc110
NEWUSER <chasevriend84@gmail.com>
2023-06-04 10:41:08 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './styles/App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import "../styles/Nav.css";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { IconButton } from '@mui/material';
+import Logo from "../images/logo.png";
+
+const Nav = () => {
+    const [nav, setNav] = useState(false);
+
+    const openNav = () => {
+        setNav(!nav);
+    }
+
+    return (
+        <>
+            <nav>
+                {/* Mobile Nav */}
+                <div className={`mobile-nav ${nav ? "open-nav" : ""}`}>
+                    <div onClick={openNav} className="mobile-nav__close">
+                        <p className='mobile-nav__XBtn'>X</p>
+                    </div>
+                    <ul className='mobile-nav__links'>
+                        <li>
+                            <Link onClick={openNav} to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link onClick={openNav} to="/cart">
+                                Cart
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                {/* Desktop Nav */}
+                <div className="nav">
+                    <div className="nav__left">
+                        <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                            {/* <p className="nav__logo"><img src={Logo} alt='' className='nav__logo' /></p> */}
+                            <p className='nav__logo'>Spider-Man 2</p>
+                        </Link>
+                    </div>
+                    <div className="nav__btns">
+                        <Link className="nav__btnSignIn" to="/">
+                            Home
+                        </Link>
+                        <Link className="" to="/cart">
+                            <IconButton>
+                                <ShoppingCartIcon
+                                    fontSize='large'
+                                />
+                            </IconButton>
+                        </Link>
+                    </div>
+
+                    {/* mobile nav  */}
+                    <div className="mobile-nav__hamburger" onClick={openNav}>
+                        <svg viewBox="0 0 100 80" width="40" height="40" className="mobile-nav__hamburgerIcon">
+                            <rect width="100" height="15"></rect>
+                            <rect y="30" width="100" height="15"></rect>
+                            <rect y="60" width="100" height="15"></rect>
+                        </svg>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Nav;
